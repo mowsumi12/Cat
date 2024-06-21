f20e83b06c07220e4c2617c04b568a0b2a70378b
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-06-21 13:01:07 +0600

    Create idp.js

---
+++ scripts/commands/idp.js
@@ -0,0 +1,28 @@
+
+module.exports.config = {
+	name: "idp",
+	version: "30.0.10",
+	Permission: 0,
+	credits: "nazrul",
+	prefix: true, 
+	description: "Random Image",
+	category: "random img",
+	usages: "",
+	cooldowns: 5
+};
+
+module.exports.run = async ({ api, event }) => {
+	const axios = require('axios');
+	const request = require('request');
+	const fs = require("fs");
+	axios.get('https://dark-rulex-vip.0xanupx0.repl.co/dp').then(res => {
+//	let ext = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
+	let callback = function () {
+					api.sendMessage({
+						body: ``,
+						attachment: fs.createReadStream(__dirname + `/cache/anear.jpg`)
+					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anear.jpg`), event.messageID);
+				};
+				request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/anear.jpg`)).on("close", callback);
+			})
+}
