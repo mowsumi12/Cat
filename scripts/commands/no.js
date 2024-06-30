10a59b737b7f43aa4f59199ae14b7bc90f3877d9
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-06-30 21:25:55 +0600

    Update no.js

--- scripts/commands/no.js
+++ scripts/commands/no.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 module.exports.config = {
-	    name: "🥺",
-    version: "1.1.0",
-    permission: 0,
-    credits: "nazrul",
-    description: "noprefix",
-    prefix: true,
-    category: "commands",
-    usages: "🥺",
-    cooldowns: 5,
+  name: "nazrul4",
+    version: "1.0.1",
+  permission: 0,
+  credits: "nazrul",
+  
+  description: "hihihihi",
+  category: "nazrul",
+  usages: "nazrul",
+    cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("💯")==0 || event.body.indexOf("💯")==0 || event.body.indexOf("💯")==0 || event.body.indexOf("💯")==0) {
-		var msg = {
-				body: "মুখে বলা কথা কাজে করে দেখাতে হবে তবেই মানুষের বিশ্বাসভাজন হতে পারবে🤠 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----",
-				attachment: fs.createReadStream(__dirname + `/noprefix/bot11.mp3`)
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🤦‍♂️", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0 || event.body.indexOf("🥲")==0) {
+    var msg = {
+        body: "-কারো মনে জায়গা পাই আর না পাই -!!🙂🥀\nকবরস্থানে একদিন ঠিকই জায়গা হয়ে যাবে ইনশাআল্লাহ্ ❞!🥲😔\n===== 𝐍𝐀𝐙𝐑𝐔𝐋 𝐁𝐎𝐓 =====",
+        attachment: fs.createReadStream(__dirname + `/nazrul/nazrul3.mp3`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🥲", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-                                                       }
+  }
