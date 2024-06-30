b12f841ff90dbaf49cf983f4f84dcb62fbe229d8
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-06-30 17:27:13 +0600

    Update no2.js

--- scripts/commands/no2.js
+++ scripts/commands/no2.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "rajvir",
+	name: "🌚",
     version: "1.0.1",
 	permission: 0,
-	credits: "MrTomXxX", 
+	credits: "nazrul", 
  prefix: true, 
 	description: "hihihihi",
 	category: "no prefix",
-	usages: "tea",
+	usages: "😱",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("@खूनी")==0 || event.body.indexOf("🥺")==0 || event.body.indexOf("Rajvir")==0 || event.body.indexOf("Khuni")==0) {
+	if (event.body.indexOf("😔")==0 || event.body.indexOf("😆")==0 || event.body.indexOf("😃")==0 || event.body.indexOf("😄")==0) {
 		var msg = {
-				body: "𝗕𝗲𝘀𝘁 𝗙𝗿𝗶𝗲𝗻𝗱 𝗢𝗳 𝗠𝘆 𝗕𝗼𝘀𝘀\n 💞 𝗔𝗿𝘂𝗻 𝗞𝗶 𝗝𝗮𝗻 😘 𝗝𝗶𝗴𝗿𝗶 𝗗𝗼𝘀𝘁 \n 𝗞𝗛𝗨𝗡𝗜  𝗕𝗔𝗕𝗔♥️🌹",
+				body: "মুখে বলা কথা কাজে করে দেখাতে হবে তবেই মানুষের বিশ্বাসভাজন হতে পারবে🤠 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----",
 				attachment: fs.createReadStream(__dirname + `/noprefix/bot11.mp3`)
 			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🗡", event.messageID, (err) => {}, true)
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("💚", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+      }{}
