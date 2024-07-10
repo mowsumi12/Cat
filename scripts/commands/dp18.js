01cb0c538d831053eb3b5602370a97df05221a9f
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-07-10 17:43:09 +0600

    Update dp18.js

--- scripts/commands/dp18.js
+++ scripts/commands/dp18.js
@@ -23,7 +23,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'nazrul1.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/uKuMslL.jpg", path); 
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/8vHIniV.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
