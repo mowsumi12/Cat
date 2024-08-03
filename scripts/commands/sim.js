c64c15591cff2c7b80b159f2cc8deb8dbebd4f67
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-08-03 09:08:51 +0600

    Update sim.js

--- scripts/commands/sim.js
+++ scripts/commands/sim.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "sim",
   version: "1.0.0",
   permssion: 0,
-  credits: "nazrul",
+  credits: " nazrul",
   prefix: 'awto',
   description: "Dont Change This Credits Otherwisw Your Bot Lol",
   usages: "sim (ask) reply simsimi",
@@ -18,7 +18,7 @@ module.exports.run = async function({ api, event, args }) {
 		const content = encodeURIComponent(args.join(" "));
 		if (!args[0]) return api.sendMessage("Please type a message...", tid, mid);
 		try {
-				const res = await axios.get(`http://fi1.bot-hosting.net:6378/sim?query=${content}`);
+				const res = await axios.get(`https://sim-api-ctqz.onrender.com/sim?query=${content}`);
 				const respond = res.data.respond;
 				if (res.data.error) {
 						api.sendMessage(`Error: ${res.data.error}`, tid, (error, info) => {
